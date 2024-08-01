4468cdcbdf9f0b7f06d664154c3f15bff1b46ddd
anykey <billykidd@mail.ru>
2024-08-01 20:58:32 +0300

    PR_8

---
+++ src/types/index.ts
@@ -0,0 +1,33 @@
+// интерфейс карточки товара
+export interface IProduct {
+	id: string;
+    description: string;
+    image: string;
+    title: string;
+    category: string;
+    price: number | null;
+}
+
+
+
+// интерфейс заказа
+export interface IOrder {
+    payment: string;
+    email: string;
+    phone: string;
+    address: string;
+    total: number
+    items: IProduct[];
+}    
+
+// интерфейс ответа сервера при отправке заказа
+export interface IOrderResponse {
+    id: string;
+    total: number;
+  }
+
+// интерфейс списка товаров  
+export interface IProductList {
+    total: number; 
+    items: IProduct[];
+}
